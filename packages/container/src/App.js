b389bfea5b6034b902b704166fe9e912b0ac98ec
Praveen Kumar <praveenkumar@Praveens-MacBook-Pro.local>
2025-07-18 22:32:19 +0530

    Adds Header component and integrates routing
    Introduces a new Header component for improved navigation.
    
    Integrates BrowserRouter to manage routing within the application.
    
    Enhances user experience by providing login/logout functionality in the header.
    
    This update lays the groundwork for future routing features.

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
 export default () => {
   return (
+    <BrowserRouter>
     <div>
-      <h1>Container App </h1>
-      <br />
+      <Header />
       <MarketingApp />
     </div>
+    </BrowserRouter>
   );
 }
