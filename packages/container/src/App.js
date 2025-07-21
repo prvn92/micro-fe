172201dd6b030b06cfa0da2ac94e1c8b9c769067
Praveen Kumar <praveenkumar@Praveens-MacBook-Pro.local>
2025-07-22 01:48:14 +0530

    Adds dashboard module with AWS deployment workflow
    Introduces a new dashboard module, including configuration for development and production environments using Webpack.
    
    Integrates the dashboard into the existing application structure and sets up an AWS deployment workflow, ensuring automatic deployment on pushes to the main branch.
    
    Enhances the application by allowing users to access the dashboard through a dedicated route, improving user experience and application functionality.

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,21 +1,29 @@
-import React, {lazy, Suspense, useState} from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import React, {lazy, Suspense, useState, useEffect} from 'react';
+import { Router, Route, Redirect } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+import { createBrowserHistory } from 'history';
 import Header from './components/Header';
 import Progress from './components/Progress';
 
 const MarketingApp= lazy(() => import('./components/MarketingApp'));
 const AuthApp = lazy(() => import('./components/AuthApp')); 
-
+const DashboardApp = lazy(() => import('./components/DashboardApp')); 
+const history = createBrowserHistory()
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co',
 });
 
 export default () => {
   const [isSingedIn, setIsSignedIn] = useState(false);
+  useEffect(() => {
+    if(isSingedIn){
+      history.push('/dashboard')
+    }
+  }, [isSingedIn]);
+
   return (
     <StylesProvider generateClassName={generateClassName} injectFirst>
-      <BrowserRouter>
+      <Router history={history}>
         <div>
           <Header isSignedIn={isSingedIn} onSignOut={() => setIsSignedIn(false)}/>
           <Suspense fallback={<Progress />}>
@@ -23,13 +31,17 @@ export default () => {
               <Route path="/auth" >
                 <AuthApp onSignIn={() => setIsSignedIn(true)}/>
               </Route>
+               <Route path="/dashboard" >
+                {!isSingedIn && <Redirect to="/" />}
+                {<DashboardApp isSignedIn={isSingedIn} />}
+              </Route>
               <Route path="/">
                 <MarketingApp isSignedIn={isSingedIn} />
               </Route>
             </switch>
           </Suspense>
         </div>
-      </BrowserRouter>
+      </Router>
     </StylesProvider>
   );
 }
